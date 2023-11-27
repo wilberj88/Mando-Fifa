03f6bf5567bf1cb0747b552c3e212ded8c2161e5
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-27 21:35:23 +0100

    Update 1_home.py

--- 1_home.py
+++ 1_home.py
@@ -1,5 +1,31 @@
 import streamlit as st
 import pandas as pd
+import webbrowser
+import datetime
 
-df_data = pd.read_csv("dataset/CLEAN_FIFA23_official_data.csv", index_col=0)
-st.session_state["data"] = df_data
+if "data" not in st.session_state:
+  df_data = pd.read_csv("dataset/CLEAN_FIFA23_official_data.csv", index_col=0)
+  df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
+  df_data = df_data[df_data["Value(£)"] > 0]
+  df_data = df_data.sort_values(by="Overall", ascending=False)
+  st.session_state["data"] = df_data
+
+st.write("""
+# FIFA23 OFFICIAL DATASET! ⚽
+""")
+
+st.sidebar.markdown("Desarrollado por [Novus Mando] (www.novussolutions.io)")
+
+btn = st.button("Accede a los datos originales desde Kaggle")
+if btn:
+  webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data/")
+
+st.markdown(
+  """
+  The Football Player Dataset from 2017 to 2023 provides comprehensive information about professional football players. 
+  The dataset contains a wide range of attributes, including player demographics, physical characteristics, playing statistics, 
+  contract details, and club affiliations. With over 17,000 records, this dataset offers a valuable resource for football analysts, 
+  researchers, and enthusiasts interested in exploring various aspects of the footballing world, as it allows for studying player 
+  attributes, performance metrics, market valuation, club analysis, player positioning, and player development over time.
+  """
+)
