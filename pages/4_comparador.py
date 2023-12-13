e96957f7d2bb8ceb3ec6e9d2a50c22ef08ff8cc3
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-12-13 16:42:10 +0100

    Update 4_comparador.py

--- pages/4_comparador.py
+++ pages/4_comparador.py
@@ -7,9 +7,20 @@ st.set_page_config(
 )
 
 df_data = st.session_state["data"]
+clubes = df_data["Club"].value_counts().index
+
+
 
 st.title("Compara jugadores 🔎")
 st.header("Selecciona al jugador 1")
+club1 = st.selectbox("Club 1", clubes)
 
+df_players1 = df_data[df_data["Club"] == club1]
+players1 = df_players1["Name"].value_counts().index
+player1 = st.selectbox("Jugador 1", players1)
 
 st.header("Selecciona al jugador 2")
+club2 = st.selectbox("Club 2", clubes)
+df_players2 = df_data[df_data["Club"] == club2]
+players2 = df_players2["Name"].value_counts().index
+player2 = st.selectbox("Jugador 2", players2)
