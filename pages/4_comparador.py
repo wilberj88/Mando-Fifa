522c9e36bc9f1e6e649793efd9cb60ef00706bf9
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-12-13 17:13:12 +0100

    Update 4_comparador.py

--- pages/4_comparador.py
+++ pages/4_comparador.py
@@ -13,14 +13,60 @@ clubes = df_data["Club"].value_counts().index
 
 st.title("Compara jugadores 🔎")
 st.header("Selecciona al jugador 1")
-club1 = st.selectbox("Club 1", clubes)
+club1 = st.selectbox("Club 1", clubes, index=None)
 
 df_players1 = df_data[df_data["Club"] == club1]
 players1 = df_players1["Name"].value_counts().index
-player1 = st.selectbox("Jugador 1", players1)
+player1 = st.selectbox("Jugador 1", players1, index=None)
 
 st.header("Selecciona al jugador 2")
-club2 = st.selectbox("Club 2", clubes)
+club2 = st.selectbox("Club 2", clubes, index=None)
 df_players2 = df_data[df_data["Club"] == club2]
 players2 = df_players2["Name"].value_counts().index
-player2 = st.selectbox("Jugador 2", players2)
+player2 = st.selectbox("Jugador 2", players2, index=None)
+
+
+if player1 and player2:
+  st.subheader("Estadísticas Jugador 1")
+  player1_stats = df_data[df_data["Name"] == player1].iloc[0]
+  st.image(player1_stats["Photo"])
+  st.title(f"{player1_stats['Name']}")
+  
+  st.markdown(f"**Club:** {player1_stats['Club']}")
+  st.markdown(f"**Posición:** {player1_stats['Position']}")
+  
+  col1, col2, col3, col4 = st.columns(4)
+  col1.markdown(f"**Edad:** {player_stats1['Age']}")
+  col2.markdown(f"**Altura:** {player1_stats['Height(cm.)']/100}")
+  col3.markdown(f"**Peso:** {player1_stats['Weight(lbs.)']*0.453:.2f}")
+  
+  st.divider()
+  st.subheader(f"Overal {player1_stats['Overall']}")
+  st.progress(int(player1_stats['Overall']))
+  
+  col1, col2, col3 = st.columns(3)
+  col1.metric(label="Valor de mercado", value=f"£ {player1_stats['Value(£)']:,}")
+  col2.metric(label="Salario mensual", value=f"£ {player1_stats['Wage(£)']:,}")
+  col3.metric(label="Cláusula de recisión", value=f"£ {player1_stats['Release Clause(£)']:,}")
+    
+  st.subheader("Estadísticas Jugador 2")
+  player2_stats = df_data[df_data["Name"] == player2].iloc[0]
+  st.image(player2_stats["Photo"])
+  st.title(f"{player2_stats['Name']}")
+  
+  st.markdown(f"**Club:** {player2_stats['Club']}")
+  st.markdown(f"**Posición:** {player2_stats['Position']}")
+  
+  col1, col2, col3, col4 = st.columns(4)
+  col1.markdown(f"**Edad:** {player2_stats['Age']}")
+  col2.markdown(f"**Altura:** {player2_stats['Height(cm.)']/100}")
+  col3.markdown(f"**Peso:** {player2_stats['Weight(lbs.)']*0.453:.2f}")
+  
+  st.divider()
+  st.subheader(f"Overal {player_stats['Overall']}")
+  st.progress(int(player_stats['Overall']))
+  
+  col1, col2, col3 = st.columns(3)
+  col1.metric(label="Valor de mercado", value=f"£ {player_stats['Value(£)']:,}")
+  col2.metric(label="Salario mensual", value=f"£ {player_stats['Wage(£)']:,}")
+  col3.metric(label="Cláusula de recisión", value=f"£ {player_stats['Release Clause(£)']:,}")
